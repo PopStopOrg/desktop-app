83ce92ec96ec5f0c78461ced3f798f24bcd28949
Jose Yanez <yanezhjs@gmail.com>
2025-01-31 19:26:12 -0700

    minor fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,12 +9,13 @@ const Navbar: React.FC = () => {
     <nav className="navbar">
       <HamMenu />
       <div className="container">
-        <img src={logo} alt="logo" className="logo" />
-        <h1>Register 1</h1>
         <InfoTags number={3} description="New" color="#e34a4a" />
         <InfoTags number={5} description="Process" color="#F3BF45" />
         <InfoTags number={10} description="Ready" color="#61BB34" />
         <InfoTags number={2} description="Served" color="#4C3B4D" />
+        
+        <h1>Register 1</h1>
+        <img src={logo} alt="logo" className="logo" />
       </div>
     </nav>
   );
